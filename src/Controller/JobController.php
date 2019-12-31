034f7907a9e655144dd8064e57956bf46c3aab6a
thanh.nguyenc <thanh.nguyenc@s3corp.com.vn>
2019-12-31 17:24:31 +0700

    ok

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -1,39 +1,36 @@
 <?php
 namespace App\Controller;
-
+use App\Entity\Job;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
-/**
- * @Route("job")
- */
+use Symfony\Component\HttpFoundation\Response;
 class JobController extends AbstractController
 {
     /**
      * Lists all job entities.
      *
-     * @Route("/", name="job.list")
+     * @Route("/", name="job.list", methods="GET")
      *
      * @return Response
      */
-    public function list() : Response
+    public function list(): Response
     {
         $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
-
         return $this->render('job/list.html.twig', [
             'jobs' => $jobs,
         ]);
     }
+
     /**
      * Finds and displays a job entity.
      *
-     * @Route("/{id}", name="job.show")
+     * @Route("job/{id}", name="job.show", methods="GET", requirements={"id" = "\d+"})
      *
      * @param Job $job
      *
      * @return Response
      */
-    public function show(Job $job) : Response
+    public function show(Job $job): Response
     {
         return $this->render('job/show.html.twig', [
             'job' => $job,
